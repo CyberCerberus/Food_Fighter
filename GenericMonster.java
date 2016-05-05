d44ce8edc9d73dc24f693812c70bd1f196eece34
SuaveJon <suavejon@eagles.ewu.edu>
2016-05-05 12:15:10 -0700

    Added files via upload
    Here are the necessary methods for the basic attack functionality.

---
+++ GenericMonster.java
@@ -0,0 +1,8 @@
+
+public class GenericMonster extends Monster {
+	
+	public GenericMonster(String name, int health, int attackSpeed, int attackDamage){
+		super(name, health, attackSpeed, attackDamage);
+	}
+	
+}
